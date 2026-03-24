179c3ffb557a48877f571f3b94737db2c7378f48
Mbb0320 <madieb09@gmail.com>
2026-03-24 10:28:04 -0600

    after spring break

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Welcome.css";
 
-const Welcome = ({ student, onPlayGame, onLogout }) => {
+const Welcome = ({ student, onPlayGame,onOpenShop, onLogout }) => {
   const name = student?.name;
 
   return (
         <div className='wrapper welcome-bg'>
-            <h1>WELCOME{name ? `, ${name}` : ""}:</h1>
-            <p>Let's get started!</p>
+          <div className="profile-icon">
+            <img src="/raccacconie-icon.png" alt="Raccacoonie Profile" />
+          </div>
+
+          <h1>WELCOME{name ? `, ${name}` : ""}:</h1>
+          <p>Let's get started!</p>
 
       <div className="buttons">
         <button onClick={onPlayGame}>PLAY GAME</button>
          <button>ASSIGNMENTS</button>
+         <button onClick={onOpenShop}>SHOP</button>
         <button onClick={onLogout}>LOGOUT</button>
       </div>
     </div>
