4ab02ef6e633b47381a34933431864e0b5646ec6
furiousgirl919-web <furiousgirl919@gmail.com>
2026-04-23 11:03:21 -0600

    Update Game_1st_grade_subtraction.js
    Updated the arrays in 1st_grade_subtraction

--- src/scenes/Game_1st_grade_subtraction.js
+++ src/scenes/Game_1st_grade_subtraction.js
@@ -14,116 +14,29 @@ export class Game_1st_grade_subtraction extends BaseMathGameScene {
             assignmentTitle: "1st Grade Subtraction",
         });
 
-        this.problems10 = [
-          {question: "10-0", answer: 10},
-          {question: "10-1", answer: 9},
-          {question: "10-2", answer: 8},
-          {question: "10-3", answer: 7},
-          {question: "10-4", answer: 6},
-          {question: "10-5", answer: 5},
-          {question: "10-6", answer: 4},
-          {question: "10-7", answer: 3},
-          {question: "10-8", answer: 2},
-          {question: "10-9", answer: 1},
-          {question: "10-10", answer: 0}
-        ];
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+    function subtractionProblems() {
+      const allProblems = [];
 
-        this.problems9 = [
-          {question: "9-0", answer: 9},
-          {question: "9-1", answer: 8},
-          {question: "9-2", answer: 7},
-          {question: "9-3", answer: 6},
-          {question: "9-4", answer: 5},
-          {question: "9-5", answer: 4},
-          {question: "9-6", answer: 3},
-          {question: "9-7", answer: 2},
-          {question: "9-8", answer: 1},
-          {question: "9-9", answer: 0}
-        ];
+      for(let n = 10; n >= 0; n--)
+      {
+        for(let j = n; j >= 0; j--)
+        {
+          allProblems.push({
+            answer: `${n - j}`,
+            question: `${n}-${j}`
+          })
+        }
+      }
+      return allProblems;
+    };
 
-        this.problems8 = [
-          {question: "8-0", answer: 8},
-          {question: "8-1", answer: 7},
-          {question: "8-2", answer: 6},
-          {question: "8-3", answer: 5},
-          {question: "8-4", answer: 4},
-          {question: "8-5", answer: 3},
-          {question: "8-6", answer: 2},
-          {question: "8-7", answer: 1},
-          {question: "8-8", answer: 0}
-        ];
-
-        this.problems7 = [
-          {question: "7-0", answer: 7},
-          {question: "7-1", answer: 6},
-          {question: "7-2", answer: 5},
-          {question: "7-3", answer: 4},
-          {question: "7-4", answer: 3},
-          {question: "7-5", answer: 2},
-          {question: "7-6", answer: 1},
-          {question: "7-7", answer: 0}
-        ];
-
-        this.problems6 = [
-          {question: "6-0", answer: 6},
-          {question: "6-1", answer: 5},
-          {question: "6-2", answer: 4},
-          {question: "6-3", answer: 3},
-          {question: "6-4", answer: 2},
-          {question: "6-5", answer: 1},
-          {question: "6-6", answer: 0}
-        ];
-
-        this.problems5 = [
-          {question: "5-0", answer: 5},
-          {question: "5-1", answer: 4},
-          {question: "5-2", answer: 3},
-          {question: "5-3", answer: 2},
-          {question: "5-4", answer: 1},
-          {question: "5-5", answer: 0}
-        ];
-
-        this.problems4 = [
-          {question: "4-0", answer: 4},
-          {question: "4-1", answer: 3},
-          {question: "4-2", answer: 2},
-          {question: "4-3", answer: 1},
-          {question: "4-4", answer: 0}
-        ];
-
-        this.problems3 = [
-          {question: "3-0", answer: 3},
-          {question: "3-1", answer: 2},
-          {question: "3-2", answer: 1},
-          {question: "3-3", answer: 0}
-        ];
-
-        this.problems2 = [
-          {question: "2-0", answer: 2},
-          {question: "2-1", answer: 1},
-          {question: "2-2", answer: 0}
-        ];
-
-        this.problems1 = [
-          {question: "1-0", answer: 1},
-          {question: "1-1", answer: 0}
-        ];
-
-        this.allProblems = [
-          ...this.problems10,
-          ...this.problems9,
-          ...this.problems8,
-          ...this.problems7,
-          ...this.problems6,
-          ...this.problems5,
-          ...this.problems4,
-          ...this.problems3,
-          ...this.problems2,
-          ...this.problems1,
-        ];
-
-        this.configuredProblems = this.getConfiguredProblems(this.allProblems);
-
+    this.problemsSubtraction = subtractionProblems();
+   
+    this.configuredProblems = this.getConfiguredProblems(this.problemsSubtraction);
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
         for (let i = 1; i <= 7; i++) {
             const y = 50 + (i - 1) * 100;
 
